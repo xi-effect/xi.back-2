144100c63d29148ed45d48124c01c25082dd56f9
niqzart <qwert45hi@yandex.ru>
2024-04-16 01:18:53 +0300

    feat: test infrastructure for mub

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
+from collections.abc import Iterator
+
 import pytest
+from fastapi.testclient import TestClient
+
+from app.common.config import MUB_KEY
+from app.main import app
 
 pytest_plugins = (
     "anyio",
@@ -11,3 +17,15 @@ pytest_plugins = (
 @pytest.fixture(scope="session")
 def anyio_backend() -> str:
     return "asyncio"
+
+
+@pytest.fixture(scope="session")
+def client() -> Iterator[TestClient]:
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope="session")
+def mub_client() -> Iterator[TestClient]:
+    with TestClient(app, headers={"X-MUB-Secret": MUB_KEY}) as client:
+        yield client
