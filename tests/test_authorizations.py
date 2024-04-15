76349884e685f90df69bcb68f04afe4f0660d49f
niqzart <qwert45hi@yandex.ru>
2024-04-16 01:39:28 +0300

    feat: common test for mub authorization

---
+++ tests/test_authorizations.py
@@ -0,0 +1,35 @@
+import pytest
+from faker import Faker
+from starlette.testclient import TestClient
+
+from tests.common.assert_contains_ext import assert_response
+
+pytestmark = pytest.mark.anyio
+
+
+@pytest.mark.parametrize(
+    "pass_token",
+    [
+        pytest.param(False, id="missing_token"),
+        pytest.param(True, id="invalid_token"),
+    ],
+)
+@pytest.mark.parametrize(
+    ("method", "path"),
+    [
+        pytest.param("POST", "/mub/communities/", id="create-community"),
+    ],
+)
+async def test_requesting_mub_invalid_key(
+    faker: Faker,
+    client: TestClient,
+    pass_token: bool,
+    method: str,
+    path: str,
+) -> None:
+    headers = {"X-MUB-Secret": faker.pystr()} if pass_token else None
+    assert_response(
+        client.request(method, path, headers=headers),
+        expected_code=401,
+        expected_json={"detail": "Invalid key"},
+    )
