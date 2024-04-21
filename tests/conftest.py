f947af27105c0a8cf0101b6828b6607fd2540e65
niqzart <qwert45hi@yandex.ru>
2024-04-21 23:17:50 +0300

    feat: authorization fixtures in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,17 @@ from collections.abc import Iterator
 
 import pytest
 from fastapi.testclient import TestClient
+from httpx import Headers
 
 from app.common.config import MUB_KEY
+from app.common.dependencies.authorization_dep import (
+    AUTH_SESSION_ID_HEADER_NAME,
+    AUTH_USER_ID_HEADER_NAME,
+    AUTH_USERNAME_HEADER_NAME,
+    ProxyAuthData,
+)
 from app.main import app
+from tests.common.polyfactory_ext import BaseModelFactory
 
 pytest_plugins = (
     "anyio",
@@ -23,6 +31,37 @@ def anyio_backend() -> str:
 def client() -> Iterator[TestClient]:
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture(scope="session")
+def authorized_client_base() -> Iterator[TestClient]:
+    with TestClient(app) as client:
+        yield client
+
+
+class ProxyAuthDataFactory(BaseModelFactory[ProxyAuthData]):
+    __model__ = ProxyAuthData
+
+
+@pytest.fixture()
+def proxy_auth_data() -> ProxyAuthData:
+    return ProxyAuthDataFactory.build()
+
+
+@pytest.fixture()
+def authorized_client(
+    authorized_client_base: TestClient,
+    proxy_auth_data: ProxyAuthData,
+) -> Iterator[TestClient]:
+    authorized_client_base.headers = Headers(
+        {
+            AUTH_SESSION_ID_HEADER_NAME: str(proxy_auth_data.session_id),
+            AUTH_USER_ID_HEADER_NAME: str(proxy_auth_data.user_id),
+            AUTH_USERNAME_HEADER_NAME: proxy_auth_data.username,
+        }
+    )
+    yield authorized_client_base
+    authorized_client_base.headers = Headers()
 
 
 @pytest.fixture(scope="session")
