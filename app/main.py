68058ca243a462aa47a9059c80ad171d4eb28ccf
niqzart <qwert45hi@yandex.ru>
2024-08-20 17:34:12 +0300

    feat: install and setup tmexio

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
+import logging
 from collections.abc import AsyncIterator, Awaitable, Callable
 from contextlib import AsyncExitStack, asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
+from pydantic import ValidationError
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.staticfiles import StaticFiles
+from tmexio import TMEXIO, AsyncSocket, EventException
 
 from app import communities, posts, storage
 from app.common.config import (
@@ -16,8 +19,38 @@ from app.common.config import (
     sessionmaker,
 )
 from app.common.config_bdg import posts_bridge
+from app.common.dependencies.authorization_dep import authorize_from_wsgi_environ
 from app.common.sqlalchemy_ext import session_context
 from app.common.starlette_cors_ext import CorrectCORSMiddleware
+
+tmex = TMEXIO(
+    async_mode="asgi",
+    transports=["websocket"],
+    cors_allowed_origins="*",
+    logger=True,
+    engineio_logger=True,
+)
+
+
+class NoPingPongFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:  # noqa: FNE005
+        return not (
+            "Received packet PONG" in record.getMessage()
+            or "Sending packet PING" in record.getMessage()
+        )
+
+
+logging.getLogger("engineio.server").addFilter(NoPingPongFilter())
+
+
+@tmex.on_connect()
+async def connect_user(socket: AsyncSocket) -> None:
+    try:
+        auth_data = await authorize_from_wsgi_environ(socket.get_environ())
+    except ValidationError:
+        raise EventException(407, "bad")
+    await socket.save_session({"auth": auth_data})
+    await socket.enter_room(f"user-{auth_data.user_id}")
 
 
 async def reinit_database() -> None:  # pragma: no cover
@@ -73,6 +106,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.mount("/socket.io/", tmex.build_asgi_app())
 
 app.include_router(communities.router)
 app.include_router(posts.router)
