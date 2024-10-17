f57d5b05176305651d838e7511cedcc6a78908bf
niqzart <qwert45hi@yandex.ru>
2024-10-17 20:58:27 +0300

    refactor: copy pytest plugin updates from niqzart/scan-quest

--- tests/common/polyfactory_ext.py
+++ tests/common/polyfactory_ext.py
@@ -3,7 +3,7 @@ from typing import Any
 from polyfactory.factories.pydantic_factory import ModelFactory
 from pydantic import BaseModel
 
-from tests.common.types import AnyJSON
+from tests.common.types import AnyJSON, AnyKwargs
 
 
 class BaseModelFactory[T: BaseModel](ModelFactory[T]):
@@ -14,6 +14,10 @@ class BaseModelFactory[T: BaseModel](ModelFactory[T]):
     def build_json(cls, **kwargs: Any) -> AnyJSON:
         return cls.build(**kwargs).model_dump(mode="json")
 
+    @classmethod
+    def build_python(cls, **kwargs: Any) -> AnyKwargs:
+        return cls.build(**kwargs).model_dump()
+
 
 class BasePatchModelFactory[T: BaseModel](BaseModelFactory[T]):
     __is_base_factory__ = True
@@ -21,3 +25,7 @@ class BasePatchModelFactory[T: BaseModel](BaseModelFactory[T]):
     @classmethod
     def build_json(cls, **kwargs: Any) -> AnyJSON:
         return cls.build(**kwargs).model_dump(mode="json", exclude_defaults=True)
+
+    @classmethod
+    def build_python(cls, **kwargs: Any) -> AnyKwargs:
+        return cls.build(**kwargs).model_dump(exclude_defaults=True)
