049b2ef6439317f05355210271248a6d8f8e6ab3
niqzart <qwert45hi@yandex.ru>
2025-01-02 01:18:04 +0300

    feat: chat-users, last_message_read in open_chat & message reading logic

---
+++ app/messenger/dependencies/chat_users_dep.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import Depends, Path
+
+from app.common.fastapi_ext import Responses, with_responses
+from app.messenger.models.chat_users_db import ChatUser
+
+
+class ChatUserResponses(Responses):
+    CHAT_USER_NOT_FOUND = 404, "Chat-user not found"
+
+
+@with_responses(ChatUserResponses)
+async def get_chat_user_by_ids(
+    chat_id: Annotated[int, Path()], user_id: Annotated[int, Path()]
+) -> ChatUser:
+    chat_user = await ChatUser.find_first_by_kwargs(chat_id=chat_id, user_id=user_id)
+    if chat_user is None:
+        raise ChatUserResponses.CHAT_USER_NOT_FOUND
+    return chat_user
+
+
+ChatUserByIds = Annotated[ChatUser, Depends(get_chat_user_by_ids)]
