e66236897e407bcd3a4561ee05984adda8e0ed96
ByrDen <comeit@outlook.com>
2025-03-27 21:39:05 +0300

    feat: scheduler events (mub, tests, migration file)

---
+++ app/scheduler/dependencies/events_dep.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import Depends, Path
+
+from app.common.fastapi_ext import Responses, with_responses
+from app.scheduler.models.events_db import Event
+
+
+class EventResponses(Responses):
+    EVENT_NOT_FOUND = 404, "Event not found"
+
+
+@with_responses(EventResponses)
+async def get_event_by_id(event_id: Annotated[int, Path()]) -> Event:
+    event = await Event.find_first_by_id(event_id)
+    if event is None:
+        raise EventResponses.EVENT_NOT_FOUND
+    return event
+
+
+EventById = Annotated[Event, Depends(get_event_by_id)]
