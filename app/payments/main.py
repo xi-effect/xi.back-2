a3d8a6af9d4df8dec277d8490bd8fc68c64313d1
Slava <slavik3001@rambler.ru>
2025-06-14 03:38:01 +0300

    feat/payment-service
    # Conflicts:
    #	app/payments/main.py

--- app/payments/main.py
+++ app/payments/main.py
@@ -6,22 +6,23 @@ from app.common.dependencies.api_key_dep import APIKeyProtection
 from app.common.dependencies.authorization_dep import ProxyAuthorized
 from app.common.dependencies.mub_dep import MUBProtection
 from app.common.fastapi_ext import APIRouterExt
+from app.payments.routes.payments import router
 
-outside_router = APIRouterExt(prefix="/api/public/scheduler-service")
+outside_router = APIRouterExt(prefix="/api/payment-service")
 
 authorized_router = APIRouterExt(
     dependencies=[ProxyAuthorized],
-    prefix="/api/protected/scheduler-service",
+    prefix="/api/protected/payment-service",
 )
 
 mub_router = APIRouterExt(
     dependencies=[MUBProtection],
-    prefix="/mub/scheduler-service",
+    prefix="/mub/payment-service",
 )
 
 internal_router = APIRouterExt(
     dependencies=[APIKeyProtection],
-    prefix="/internal/scheduler-service",
+    prefix="/internal/payment-service",
 )
 
 
@@ -35,3 +36,4 @@ api_router.include_router(outside_router)
 api_router.include_router(authorized_router)
 api_router.include_router(mub_router)
 api_router.include_router(internal_router)
+api_router.include_router(router)
