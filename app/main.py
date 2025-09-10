dea59a1ad68384afe3c125ae4759155a93294d89
niqzart <qwert45hi@yandex.ru>
2025-09-10 23:17:35 +0300

    fix: add autocomplete_bridge to exit stack

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ from app import (
 )
 from app.common.config import Base, engine, sessionmaker, settings
 from app.common.config_bdg import (
+    autocomplete_bridge,
     communities_bridge,
     messenger_bridge,
     posts_bridge,
@@ -92,6 +93,7 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
         await reinit_database()
 
     async with AsyncExitStack() as stack:
+        await stack.enter_async_context(autocomplete_bridge.client)
         await stack.enter_async_context(communities_bridge.client)
         await stack.enter_async_context(messenger_bridge.client)
         await stack.enter_async_context(posts_bridge.client)
