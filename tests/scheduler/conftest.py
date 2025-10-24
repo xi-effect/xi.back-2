12affe2698f55d9dc620e0c0cce8779ccf796443
ByrDen <comeit@outlook.com>
2025-10-24 04:28:52 +0300

    feat: classroom events rst remove: events MUB

--- tests/scheduler/conftest.py
+++ tests/scheduler/conftest.py
@@ -1,28 +1,68 @@
 import pytest
+from faker import Faker
+from starlette.testclient import TestClient
 
-from app.scheduler.models.events_db import Event
+from app.common.dependencies.authorization_dep import ProxyAuthData
+from app.scheduler.models.events_db import ClassroomEvent
 from tests.common.active_session import ActiveSession
 from tests.common.types import AnyJSON
+from tests.factories import ProxyAuthDataFactory
 from tests.scheduler import factories
 
 
 @pytest.fixture()
-async def event(
+def tutor_auth_data() -> ProxyAuthData:
+    return ProxyAuthDataFactory.build()
+
+
+@pytest.fixture()
+def tutor_client(client: TestClient, tutor_auth_data: ProxyAuthData) -> TestClient:
+    return TestClient(client.app, headers=tutor_auth_data.as_headers)
+
+
+@pytest.fixture()
+def student_auth_data() -> ProxyAuthData:
+    return ProxyAuthDataFactory.build()
+
+
+@pytest.fixture()
+def student_client(client: TestClient, student_auth_data: ProxyAuthData) -> TestClient:
+    return TestClient(client.app, headers=student_auth_data.as_headers)
+
+
+@pytest.fixture()
+def classroom_id(faker: Faker) -> int:
+    return faker.random_int(1, 1000)
+
+
+@pytest.fixture()
+def other_classroom_id(faker: Faker, classroom_id: int) -> int:
+    return faker.random_int(classroom_id + 1, classroom_id + 1000)
+
+
+@pytest.fixture()
+async def classroom_event(
+    active_session: ActiveSession, classroom_id: int
+) -> ClassroomEvent:
+    async with active_session():
+        return await ClassroomEvent.create(
+            **factories.ClassroomEventInputFactory.build_python(),
+            classroom_id=classroom_id,
+        )
+
+
+@pytest.fixture()
+def classroom_event_data(classroom_event: ClassroomEvent) -> AnyJSON:
+    return ClassroomEvent.ResponseSchema.model_validate(
+        classroom_event, from_attributes=True
+    ).model_dump(mode="json")
+
+
+@pytest.fixture()
+async def deleted_classroom_event_id(
     active_session: ActiveSession,
-) -> Event:
+    classroom_event: ClassroomEvent,
+) -> int:
     async with active_session():
-        return await Event.create(**factories.EventInputFactory.build_python())
-
-
-@pytest.fixture()
-async def event_data(
-    event: Event,
-) -> AnyJSON:
-    return Event.ResponseSchema.model_validate(event).model_dump(mode="json")
-
-
-@pytest.fixture()
-async def deleted_event_id(active_session: ActiveSession, event: Event) -> int:
-    async with active_session():
-        await event.delete()
-    return event.id
+        await classroom_event.delete()
+    return classroom_event.id
--- tests/scheduler/factories.py
+++ tests/scheduler/factories.py
@@ -2,14 +2,27 @@ from datetime import timezone
 
 from polyfactory import PostGenerated
 
-from app.scheduler.models.events_db import Event
+from app.scheduler.models.events_db import ClassroomEvent
 from tests.common.polyfactory_ext import BaseModelFactory
 
 
-class EventInputFactory(BaseModelFactory[Event.InputSchema]):
-    __model__ = Event.InputSchema
+class ClassroomEventInputFactory(BaseModelFactory[ClassroomEvent.InputSchema]):
+    __model__ = ClassroomEvent.InputSchema
+
     ends_at = PostGenerated(
         lambda _, values: BaseModelFactory.__faker__.date_time_between(
             start_date=values["starts_at"], end_date="+120m", tzinfo=timezone.utc
+        )
+    )
+
+
+class ClassroomEventInvalidTimeFrameInputFactory(
+    BaseModelFactory[ClassroomEvent.InputSchema]
+):
+    __model__ = ClassroomEvent.InputSchema
+
+    ends_at = PostGenerated(
+        lambda _, values: BaseModelFactory.__faker__.date_time(
+            end_datetime=values["starts_at"], tzinfo=timezone.utc
         )
     )
