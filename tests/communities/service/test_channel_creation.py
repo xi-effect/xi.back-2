304025c3e222f617deaf6227941ca61fb55b558b
niqzart <qwert45hi@yandex.ru>
2025-11-03 05:57:54 +0300

    refactor: remove old storage & old tutor materials

--- tests/communities/service/test_channel_creation.py
+++ tests/communities/service/test_channel_creation.py
@@ -1,7 +1,6 @@
 import json
 from collections.abc import Callable
 from random import randint
-from uuid import uuid4
 
 import pytest
 from httpx import Request, Response
@@ -11,8 +10,6 @@ from starlette import status
 
 from app.common.config import Base, settings
 from app.common.schemas.messenger_sch import ChatAccessKind
-from app.common.schemas.storage_sch import StorageAccessGroupKind
-from app.communities.models.board_channels_db import BoardChannel
 from app.communities.models.call_channels_db import CallChannel
 from app.communities.models.channels_db import Channel, ChannelType
 from app.communities.models.chat_channels_db import ChatChannel
@@ -63,87 +60,6 @@ async def test_post_channel_creation(
         expected_headers={"X-Api-Key": settings.api_key},
         expected_path=f"/internal/post-service/post-channels/{channel.id}/",
         expected_json={"community_id": community.id},
-    )
-
-
-def create_access_group_mock_side_effect_factory(
-    access_group_id: str,
-) -> Callable[[Request], Response]:
-    def create_access_group_mock_side_effect(request: Request) -> Response:
-        try:
-            json_data = json.loads(request.content)
-        except (UnicodeDecodeError, json.JSONDecodeError):
-            pytest.fail("Invalid request body")
-
-        assert_contains(json_data, {"kind": StorageAccessGroupKind, "related_id": str})
-        return Response(
-            status_code=status.HTTP_201_CREATED,
-            json={
-                "id": access_group_id,
-                "kind": json_data["kind"],
-                "related_id": json_data["related_id"],
-            },
-        )
-
-    return create_access_group_mock_side_effect
-
-
-async def test_board_channel_creation(
-    active_session: ActiveSession,
-    storage_respx_mock: MockRouter,
-    community: Community,
-    channel_parent_category_id: int | None,
-) -> None:
-    channel_raw_data: Channel.InputSchema = factories.ChannelInputFactory.build(
-        kind=ChannelType.BOARD
-    )
-
-    access_group_id = str(uuid4())
-    ydoc_id = str(uuid4())
-
-    create_access_group_mock = storage_respx_mock.post(path="/access-groups/").mock(
-        side_effect=create_access_group_mock_side_effect_factory(access_group_id)
-    )
-    create_ydoc_mock = storage_respx_mock.post(
-        path=f"/access-groups/{access_group_id}/ydocs/"
-    ).respond(status_code=status.HTTP_201_CREATED, json={"id": ydoc_id})
-
-    async with active_session():
-        channel = await channels_svc.create_channel(
-            community_id=community.id,
-            category_id=channel_parent_category_id,
-            data=channel_raw_data,
-        )
-
-    assert_contains(
-        channel,
-        {
-            **channel_raw_data.model_dump(),
-            "id": int,
-            "community_id": community.id,
-            "category_id": channel_parent_category_id,
-            "list_id": [community.id, channel_parent_category_id],
-        },
-    )
-
-    async with active_session():
-        board_channel = await BoardChannel.find_first_by_id(channel.id)
-        assert_contains(
-            board_channel,
-            {"access_group_id": access_group_id, "ydoc_id": ydoc_id},
-        )
-
-    assert_last_httpx_request(
-        create_access_group_mock,
-        expected_headers={"X-Api-Key": settings.api_key},
-        expected_json={
-            "kind": StorageAccessGroupKind.BOARD_CHANNEL.value,
-            "related_id": str(channel.id),
-        },
-    )
-    assert_last_httpx_request(
-        create_ydoc_mock,
-        expected_headers={"X-Api-Key": settings.api_key},
     )
 
 
