36f52ee7d94ed06503a2b123adc10bcceed0bae5
niqzart <qwert45hi@yandex.ru>
2025-11-09 22:16:13 +0300

    feat: support notifications in email messages & pochta

--- app/pochta/routes/email_messages_sub.py
+++ app/pochta/routes/email_messages_sub.py
@@ -22,6 +22,12 @@ KIND_TO_TEMPLATE_ID: dict[EmailMessageKind, str] = {
     EmailMessageKind.EMAIL_CONFIRMATION_V2: "05b83984-bd89-11f0-81f1-122da0a24080",
     EmailMessageKind.EMAIL_CHANGE_V2: "a25aced8-bd88-11f0-b8e4-122da0a24080",
     EmailMessageKind.PASSWORD_RESET_V2: "3b5242e2-bd89-11f0-8132-025779db5bd3",
+    EmailMessageKind.INDIVIDUAL_INVITATION_ACCEPTED_V1: "dfe2b0da-b7df-11f0-9a0e-1ecf01eb9d7d",
+    EmailMessageKind.GROUP_INVITATION_ACCEPTED_V1: "5c4927c6-b7fe-11f0-8c0e-d2544595dc68",
+    EmailMessageKind.ENROLLMENT_CREATED_V1: "8ae0674c-b7ff-11f0-a05c-d2544595dc68",
+    EmailMessageKind.CLASSROOM_CONFERENCE_STARTED_V1: "0aef5510-b800-11f0-ad49-d2544595dc68",
+    EmailMessageKind.RECIPIENT_INVOICE_CREATED_V1: "a466ca48-b800-11f0-80a2-d2544595dc68",
+    EmailMessageKind.STUDENT_RECIPIENT_INVOICE_PAYMENT_CONFIRMED_V1: "9c5cd7cc-b7fe-11f0-8d2e-d2544595dc68",
 }
 
 
@@ -47,13 +53,16 @@ async def send_email_message(
         },
     )
 
-    response_data = await unisender_go_client.send_email(
+    unisender_go_response_data = await unisender_go_client.send_email(
         UnisenderGoSendEmailRequestSchema(message=message_data)
     )
     # TODO better error handling
     for recipient_email in data.recipient_emails:
-        if recipient_email not in response_data.emails:
+        if recipient_email not in unisender_go_response_data.emails:
             logging.error(
                 f"Sending email to {recipient_email} failed",
-                extra={"full_error": response_data},
+                extra={
+                    "input_data": data,
+                    "unisender_go_response_data": unisender_go_response_data,
+                },
             )
