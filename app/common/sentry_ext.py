e496936aa5e2f29fb5524fec6b1e275920f9c737
niqzart <qwert45hi@yandex.ru>
2025-11-22 04:32:54 +0300

    fix: cleanup breadcrumbs from telegram initial logs

--- app/common/sentry_ext.py
+++ app/common/sentry_ext.py
@@ -2,8 +2,14 @@ from sentry_sdk.types import Breadcrumb, BreadcrumbHint
 
 
 def before_breadcrumb(crumb: Breadcrumb, _hint: BreadcrumbHint) -> Breadcrumb | None:
-    if crumb.get("category") == "redis":
-        redis_command = crumb.get("data", {}).get("redis.command")
-        if redis_command in {"PING", "XREADGROUP", "XGROUP CREATE"}:
+    crumb_category = crumb.get("category")
+    crumb_data = crumb.get("data", {})
+    if crumb_category == "redis":
+        redis_command = crumb_data.get("redis.command")
+        if redis_command in {"PING", "XREADGROUP", "XGROUP CREATE", "XACK"}:
+            return None
+    elif crumb_category == "httplib":
+        http_url: str = crumb_data.get("url")
+        if http_url.endswith(("setWebhook", "setMyCommands", "getMe")):
             return None
     return crumb
