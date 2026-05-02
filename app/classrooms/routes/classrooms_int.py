56eec9407349c83229cd1e80dbff6f888dfd3ae2
niqzart <qwert45hi@yandex.ru>
2026-05-02 08:51:49 +0300

    feat: schedules for user's classrooms
    t

--- app/classrooms/routes/classrooms_int.py
+++ app/classrooms/routes/classrooms_int.py
@@ -1,10 +1,18 @@
 from collections.abc import Sequence
-from typing import assert_never
+from typing import Annotated, assert_never
+
+from fastapi import Path
+from sqlalchemy import or_, select
 
 from app.classrooms.dependencies.classrooms_dep import ClassroomByID
-from app.classrooms.models.classrooms_db import GroupClassroom, IndividualClassroom
+from app.classrooms.models.classrooms_db import (
+    Classroom,
+    GroupClassroom,
+    IndividualClassroom,
+)
 from app.classrooms.models.enrollments_db import Enrollment
 from app.common.fastapi_ext import APIRouterExt
+from app.common.sqlalchemy_ext import db
 
 router = APIRouterExt(tags=["classrooms internal"])
 
@@ -23,3 +31,39 @@ async def list_classroom_student_ids(classroom: ClassroomByID) -> Sequence[int]:
             )
         case _:
             assert_never(classroom)
+
+
+@router.get(
+    path="/tutors/{tutor_id}/classroom-ids/",
+    summary="List all classroom ids for a tutor by id",
+)
+async def list_tutor_classroom_ids(
+    tutor_id: Annotated[int, Path()],
+) -> list[int]:
+    return await db.get_all_with_assumed_limit(
+        select(Classroom.id)
+        .filter_by(tutor_id=tutor_id)
+        .order_by(Classroom.created_at.desc()),
+        limit=100,
+    )
+
+
+@router.get(
+    path="/students/{student_id}/classroom-ids/",
+    summary="List all classroom ids for a student by id",
+)
+async def list_student_classroom_ids(
+    student_id: Annotated[int, Path()],
+) -> list[int]:
+    return await db.get_all_with_assumed_limit(
+        select(Classroom.id)
+        .join(Enrollment, isouter=True)
+        .filter(
+            or_(
+                IndividualClassroom.student_id == student_id,
+                Enrollment.student_id == student_id,
+            )
+        )
+        .order_by(Classroom.created_at.desc()),
+        limit=100,
+    )
--- tests/classrooms/functional/test_classrooms_int.py
+++ tests/classrooms/functional/test_classrooms_int.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic_marshals.contains import UnorderedLiteralCollection
 from starlette import status
 from starlette.testclient import TestClient
 
@@ -46,4 +47,39 @@ async def test_listing_classroom_students_classroom_not_found(
         ),
         expected_code=status.HTTP_404_NOT_FOUND,
         expected_json={"detail": "Classroom not found"},
+    )
+
+
+# TODO maybe expand
+
+
+async def test_listing_tutor_classroom_ids(
+    internal_client: TestClient,
+    tutor_user_id: int,
+    individual_classroom: IndividualClassroom,
+    group_classroom: GroupClassroom,
+) -> None:
+    assert_response(
+        internal_client.get(
+            f"/internal/classroom-service/tutors/{tutor_user_id}/classroom-ids/",
+        ),
+        expected_json=UnorderedLiteralCollection(
+            [individual_classroom.id, group_classroom.id],
+        ),
+    )
+
+
+async def test_listing_student_classroom_ids(
+    internal_client: TestClient,
+    student_user_id: int,
+    individual_classroom: IndividualClassroom,
+    enrollment: Enrollment,
+) -> None:
+    assert_response(
+        internal_client.get(
+            f"/internal/classroom-service/students/{student_user_id}/classroom-ids/",
+        ),
+        expected_json=UnorderedLiteralCollection(
+            [individual_classroom.id, enrollment.group_classroom_id],
+        ),
     )
