3611e1750af884e4f9eedb7fd9407d07381fc9c9
marlowe-agent <314417787+marlowe-agent@users.noreply.github.com>
2026-08-24 03:12:45 +0300

    feat: content service cutover plumbing

--- tests/storage_v2/factories.py
+++
@@ -1,6 +0,0 @@
-from app.common.schemas.storage_sch import StorageTokenPayloadSchema
-from tests.common.polyfactory_ext import BaseModelFactory
-
-
-class StorageTokenPayloadFactory(BaseModelFactory[StorageTokenPayloadSchema]):
-    __model__ = StorageTokenPayloadSchema
